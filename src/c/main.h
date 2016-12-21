eca20a6d6d869c890686fac6b70c3008562c277b
Luke Storry <luke@lukestorry.co.uk>
2016-12-21 20:42:40 +0000

    comic font added

--- src/c/main.h
+++ src/c/main.h
@@ -1,6 +1,8 @@
 #pragma once
 #include <pebble.h>
-static void main_window_load(Window *window);
-static void main_window_unload(Window *window);
+
 static void init();
 static void deinit();
+static void main_window_load(Window *window);
+static void main_window_unload(Window *window);
+static void update_time();
